11a627a915fe92c7c337e22b53190254ed9993cd
gorag <gorbunov.alexey@gmail.com>
2023-07-23 04:18:30 +0300

    publish photo to Telegram channel

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ import telegram
 if __name__ == "__main__":
     # "5eb87ce4ffd86e000604b337"
     bot = telegram.Bot(token="6523902154:AAEd6Q_ijplmRVPlZvbuaYBQx3cHrLFBrCw")
-    bot.send_message(chat_id="@astronomy_df", text="Hello")
+    media = telegram.InputMediaPhoto(media=open("images/nasa_epic0.png", "rb"))
+    bot.send_media_group(chat_id="@astronomy_df", media=[media])
